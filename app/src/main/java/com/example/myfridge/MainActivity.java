b7529f8b24069d41452c2f425fbf9340fca338de
st3phy831 <st3phy831@gmail.com>
2020-12-19 21:33:02 -0800

    Created and designed a fragment

--- app/src/main/java/com/example/myfridge/MainActivity.java
+++ app/src/main/java/com/example/myfridge/MainActivity.java
@@ -2,17 +2,21 @@ package com.example.myfridge;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.myfridge.fragments.ShoppingListFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
 
+    final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -25,19 +29,25 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                //Must set all of them to Shopping list Fragment so it can compile and run
                 switch(menuItem.getItemId()){
                     case R.id.action_list:
+                        fragment = new ShoppingListFragment();
                         Toast.makeText(getApplicationContext(), "ToBuy List", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_add:
+                        fragment = new ShoppingListFragment();
                         Toast.makeText(getApplicationContext(), "Add to List", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_fridge:
                     default:
+                        fragment = new ShoppingListFragment();
                         Toast.makeText(getApplicationContext(), "Fridge List", Toast.LENGTH_SHORT).show();
                         break;
 
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
