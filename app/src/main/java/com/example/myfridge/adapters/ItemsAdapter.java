290e5d4067cff920022fcb62e59aab6bd66da251
st3phy831 <st3phy831@gmail.com>
2020-12-20 13:10:26 -0800

    Created an adapter to display items in Shopping List fragment

---
+++ app/src/main/java/com/example/myfridge/adapters/ItemsAdapter.java
@@ -0,0 +1,63 @@
+package com.example.myfridge.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myfridge.R;
+
+import java.util.List;
+
+//display data from model in row of recycler view
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
+    List<String> items;
+
+    public ItemsAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //use layout inflater to inflate view
+        View buyView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        //wrap it inside view holder and return it
+        return new ViewHolder(buyView);
+    }
+
+    //taking data at particular position and putting into holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //grab item at position
+        String item = items.get(position);
+        //bind item to specified view holder
+        holder.bind(item);
+
+    }
+
+    //Gets items available
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    //container to provide easy access to view
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        //update the view inside the view holder with this data
+        public void bind(String item) {
+            tvItem.setText(item);
+
+        }
+    }
+}
--- app/src/main/java/com/example/myfridge/fragments/ShoppingListFragment.java
+++ app/src/main/java/com/example/myfridge/fragments/ShoppingListFragment.java
@@ -2,65 +2,66 @@ package com.example.myfridge.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.myfridge.R;
+import com.example.myfridge.adapters.ItemsAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ShoppingListFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class ShoppingListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "Shopping List";
+    List<String> items;
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public ShoppingListFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ShoppingListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ShoppingListFragment newInstance(String param1, String param2) {
-        ShoppingListFragment fragment = new ShoppingListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shopping_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnAdd = view.findViewById(R.id.btnAdd);
+        etItem = view.findViewById(R.id.etItem);
+        rvItems = view.findViewById(R.id.rvItems);
+
+
+        items = new ArrayList<>();
+        items.add("Buy Milk");
+        items.add("Buy Eggs");
+        items.add("Buy Flour");
+
+        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(getContext()));
+
     }
 }
